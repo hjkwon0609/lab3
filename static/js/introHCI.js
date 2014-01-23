c19f3e27cd4061c3b34d709ab556fa54e5278cc8
Mark Kwon <hjkwon0609@gmail.com>
2014-01-22 22:49:24 -0800

    commit

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,41 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("Clicked");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject);			
+}
+
+function projectClick(e){
+	console.log("Project cicked");
+	// prevent the page from reloading
+	e.preventDefault();
+
+	
+	//In an event listener, $(this) is the element that fired the event
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggleClass("hide");
+	}
+}
+
+function updateProject(e){
+	var which = $('#project').val();
+	$(which).animate({
+		width: $('#width').val()
+	});
+
+	var descrip = $('#description').val();
+	$(which + ' .project-description').text(descrip);
 }
